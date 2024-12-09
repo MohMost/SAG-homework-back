736eb65b04b15ded99075611b7cf69d43d284a43
MohMost <teemohmost2020@gmail.com>
2024-12-09 13:48:17 +0100

    test routes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import router from "./routes/booksRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use(
   cors({
     origin: "https://sag-assignment-back.vercel.app/",
