e42ac46b63e40b4dc7f5138793c67c191051635f
MohMost <teemohmost2020@gmail.com>
2024-12-09 12:56:20 +0100

    🔺(adding): vercel port

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ import cors from "cors";
 import router from "./routes/booksRoute.js";
 
 const app = express();
-const PORT = 3000;
-
-app.use(cors());
+const PORT = process.env.PORT || 3000;
+app.use(
+  cors({
+    origin: "https://sag-assignment-back.vercel.app/",
+  })
+);
 
 app.use(express.json());
 
-app.use(("/api/books", router));
+app.use("/api/books", router);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
